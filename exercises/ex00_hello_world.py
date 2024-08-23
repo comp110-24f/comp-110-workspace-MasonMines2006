e6083e9be4d721d0e43dd411cb9f336c837321e5
Mason Mines <mmines@unc.edu>
2024-08-23 11:32:15 -0400

    Final EX001 success

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,12 @@
+"""My first exercise in COMP110!"""
+
+__author__ = "730765505"
+
+
+def greet(name: str) -> str:
+    """A welcoming first function definition."""
+    return "Hello, " + name + "!"
+
+
+print(greet(name="Campers"))
+print(greet(name="Mason"))
